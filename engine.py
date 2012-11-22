bc8fcd07d3a5643e119b0761074a902ed4903f31
Luis de Bethencourt <luis@debethencourt.com>
2012-11-21 19:01:50 -0500

    comment the code

--- engine.py
+++ engine.py
@@ -31,63 +31,76 @@ from gi.repository import GES
 from gi.repository import GObject
 
 class Engine():
+    '''Interface class to Gst-Editing-Services'''
 
     def __init__(self):
-        GES.init()
+        '''Initialize the Python class'''
+        GES.init()    # we need to init GES for it to work
 
-        self.timeline = GES.Timeline()
-        self.pipeline = GES.TimelinePipeline()
+        self.timeline = GES.Timeline()   # create the central object of the Timeline
+        self.pipeline = GES.TimelinePipeline()   # convenience class to render the Timeline
+                                                 # handles the clock, bus and other things for us
         self.bus = self.pipeline.get_bus()
-        self.pipeline.add_timeline(self.timeline)
+        self.pipeline.add_timeline(self.timeline)    # we need to add the Timeline into the Pipeline
 
-        GES.Timeline.__init__(self.timeline)
+        GES.Timeline.__init__(self.timeline)    # initialize the Timeline
 
-        self.audio = GES.Track.audio_raw_new()
-        self.video = GES.Track.video_raw_new()
+        self.audio = GES.Track.audio_raw_new()    # we will have one raw Audio Track
+        self.video = GES.Track.video_raw_new()    # and one raw Video Track
 
-        self.timeline.add_track(self.audio)
+        self.timeline.add_track(self.audio)    # we add the Tracks to the Timeline
         self.timeline.add_track(self.video)
 
-        Gst.init(None)
+        Gst.init(None)    # we need to initialize Gst for it to work
 
 
     def add_file(self, file_uri):
-        src = GES.TimelineFileSource(uri=file_uri)
-        src.set_priority(1)
+        '''Function to add a file to the timeline'''
+        src = GES.TimelineFileSource(uri=file_uri)    # we create a TimelineObject from the File
+        src.set_priority(0)    # we add it to the bottom of the Timeline
 
-        layer = GES.TimelineLayer()
-        layer.add_object(src)
-        self.timeline.add_layer(layer)
+        layer = GES.TimelineLayer()    # we create a new TimelineLayer
+        layer.add_object(src)          # which will contain the new TimelineObject
+        self.timeline.add_layer(layer)    # so we can add it to the Timeline
 
-        disc = GstPbutils.Discoverer.new (50000000000)
+        disc = GstPbutils.Discoverer.new (50000000000)   # we discover the duration of the file
         info = disc.discover_uri (file_uri)
+
+        # return the duration of the clip and the pointer to the TimelineObject
         return info.get_duration(), src
 
     def play(self):
+        '''Method to change the state of the Pipeline to Playing'''
         self.pipeline.set_state(Gst.State.PLAYING)
 
     def stop(self):
+        '''Method to change the state of the Pipeline to Ready'''
         self.pipeline.set_state(Gst.State.READY)
 
     def change_object_duration(self, tlobj, duration):
+        '''Method to change the duration of the TimelineObject'''
         tlobj.set_duration(duration)
 
     def change_object_start(self, tlobj, start):
+        '''Method to change the starting position of the TimelineObject'''
         tlobj.set_start(start)
 
     def change_object_inpoint(self, tlobj, in_point):
+        '''Method to change the in point to play in the TimelineObject'''
         tlobj.set_inpoint(in_point)
 
     def change_object_priority(self, tlobj, priority):
+        '''Method to change the priority of the TimelineObject inside its TimelineLayer'''
         tlobj.set_priority(priority)
 
     def prioritize(self, in_obj):
-        '''prioritize selected object by changing its priority to 1 and the rest
-           to 0'''
-        for layer in self.timeline.get_layers():
-            for obj in layer.get_objects():
+        '''Method to raise the priority of the TimelineObject to the top'''
+        # prioritize selected object by changing its priority to 1 and the rest
+        #   to 0
+        for layer in self.timeline.get_layers():    # we run through the TimelineLayers
+            for obj in layer.get_objects():         # check if the objects inside the TimelinLayers
                 #print "obj uri ", obj.get_properties("uri")
-                if obj is in_obj:
+                if obj is in_obj:                   # are the selected TimelineObject, and if they are
                     self.change_object_priority(obj, 0)
-                else:
+                else:                               # we change priority up to 1 or set back to 0
                     self.change_object_priority(obj, 1)
--- py-ges-demo.py
+++ py-ges-demo.py
@@ -34,12 +34,15 @@ from gi.repository import GObject
 from engine import Engine
 
 class GesDemo():
-
+    '''User Interface class'''
     def __init__(self):
+        '''Initialize the User Interface and the GES Engine'''
+
         self.engine = Engine()
 
         self.clips = {}
 
+        # we import the gtk objects from glade and create our GtkStore and TreeView
         self.builder = Gtk.Builder()
         self.builder.add_from_file(os.path.join(os.path.curdir, \
             "py-ges-demo.glade"))
@@ -58,18 +61,8 @@ class GesDemo():
         self.window = self.builder.get_object("window")
         self.window.show_all()
 
-    def add_file(self, filepath):
-        idf = len(self.clips)
-
-        uri = Gst.filename_to_uri (filepath)
-        duration, tlobj = self.engine.add_file(uri)
-
-        self.timeline_store.append([idf, os.path.basename(filepath), 0,
-                                   duration, 0])
-        # clips[id] = uri, start, duration, in_point, max_duration, timelineobj
-        self.clips[idf] = [uri, 0, duration, 0, duration, tlobj]
-
     def _clip_selected(self, widget):
+        '''Method connected to the selection-changed signal of the TreeView'''
         model, row_iter = self.timeline_treeview.get_selection().get_selected()
         if row_iter is None:
             self.engine.prioritize(None)
@@ -81,6 +74,7 @@ class GesDemo():
             self._update_properties_box(idf)
 
     def _update_properties_box(self, idf):
+        '''Method to update the properties box with the information of the selected clip'''
         clip = self.clips[idf]
         self.start_entry.set_text(str(clip[1]))
         self.duration_scale.set_range(0, clip[4] - clip[3])
@@ -88,13 +82,16 @@ class GesDemo():
         self.in_point_scale.set_range(0, clip[4])
         self.in_point_scale.set_value(clip[3])
 
-    def _stop_activate_cb(self, widget):
-        self.engine.stop()
-
     def _play_activate_cb(self, widget):
+        '''Method connected to the Play button'''
         self.engine.play()
 
+    def _stop_activate_cb(self, widget):
+        '''Method connected to the Stop button'''
+        self.engine.stop()
+
     def _add_file_activated_cb(self, widget):
+        '''Method connected to the Add File button'''
         filechooser = Gtk.FileChooserDialog(action=Gtk.FileChooserAction.OPEN,
                       buttons=(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
                     Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
@@ -108,47 +105,78 @@ class GesDemo():
             filechooser.destroy()
             self.add_file(filepath)
 
+    def add_file(self, filepath):
+        '''Method that adds the file to the GtkStore and GESTimeline'''
+        idf = len(self.clips)    # id field is a counter
+
+        uri = Gst.filename_to_uri (filepath)   # conver the filepath into a valid URI
+        duration, tlobj = self.engine.add_file(uri)    # add the uri to the GESTimeline
+
+        # add to the GtkStore and internal list
+        self.timeline_store.append([idf, os.path.basename(filepath), 0,
+                                   duration, 0])
+        # clips[id] = uri, start, duration, in_point, max_duration, timelineobj
+        self.clips[idf] = [uri, 0, duration, 0, duration, tlobj]
+
     def _delete_activate_cb(self, widget):
+        '''Method connected to the Delete button'''
         print "delete"
 
     def _start_changed(self, widget):
+        '''Method connected to the Changed event of the start entry'''
         new_start = widget.get_text()
         print "new start entered", new_start
 
+        # find the selected clip
         model, row_iter = self.timeline_treeview.get_selection().get_selected()
         idf = self.timeline_store.get_value(row_iter, 0)
         self.clips[idf][1] = new_start
+        # set the new value to the Gtk TreeStore
         try:
             self.timeline_store.set_value(row_iter, 2, long(new_start))
         except:
             self.timeline_store.set_value(row_iter, 2, 0)
             new_start = 0
 
+        # change the value of the GES TimelineObject
         self.engine.change_object_start(self.clips[idf][5], long(new_start))
 
     def _duration_scale_change_value_cb(self, widget, event):
+        '''Method connected to the Changed event of the Duration scale'''
         new_duration = widget.get_value()
         print "duration scale change", new_duration
 
+        # find the selected clip
         model, row_iter = self.timeline_treeview.get_selection().get_selected()
         idf = self.timeline_store.get_value(row_iter, 0)
         self.clips[idf][2] = new_duration
+        # set the new value to the Gtk TreeStore
         self.timeline_store.set_value(row_iter, 3, long(new_duration))
+
+        # change the value of the GES TimelineObject
         self.engine.change_object_duration(self.clips[idf][5], new_duration)
 
     def _in_point_scale_change_value_cb(self, widget, event):
+        '''Method connected to the Changed event of the In Point scale'''
         new_in_point = widget.get_value()
         print "in point scale change value", new_in_point
 
+        # find the selected clip
         model, row_iter = self.timeline_treeview.get_selection().get_selected()
         idf = self.timeline_store.get_value(row_iter, 0)
         self.clips[idf][3] = new_in_point
+        # set the new value to the Gtk TreeStore
         self.timeline_store.set_value(row_iter, 4, long(new_in_point))
+
+        # change the value of the GES TimelineObject
         self.engine.change_object_inpoint(self.clips[idf][5], new_in_point)
 
     def _window_delete_event_cb(self, unused_window=None, unused_even=None):
+        '''Method connected to the Delete event of the window'''
         Gtk.main_quit
         exit(0)
 
+
+# Start the application
 demo = GesDemo()
 Gtk.main()
